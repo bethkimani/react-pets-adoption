01b5998cc070d353b8c783e3f37b9e3329ac6c87
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-03-17 00:51:42 +0300

    added the backend live link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: 'https://pets-adoption-flask-sqlite.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
