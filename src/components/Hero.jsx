4f86adfe75277b39296d0fb9ca025be30a2d02d8
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-03-27 13:42:13 +0300

    returned the web appilication the way it was

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,6 @@
 import React, { useState } from 'react';
-import Slider from 'react-slick';
 import '../App.css';
 import BookingModal from './BookingPet';
-
-// Importing carousel images
-import homepg from '../assets/images/homepg.jpg'; // Adjust the path according to your structure
-
-// Carousel images
-const carouselImages = [
-  homepg,
-  homepg,
-  homepg,
-];
 
 const Hero = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -24,26 +13,8 @@ const Hero = () => {
     setIsModalOpen(false);
   };
 
-  // Carousel settings
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
-
   return (
     <div className="hero">
-      <Slider {...settings}>
-        {carouselImages.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt={`Slide ${index + 1}`} />
-          </div>
-        ))}
-      </Slider>
       <div className="hero-text">
         <h1 className="hero-title">
           Find Your Perfect Furry Friend Today!
